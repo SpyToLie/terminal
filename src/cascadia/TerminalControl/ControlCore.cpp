SpyToLie/terminal

commit ac265aab99d5f79345074fbff66c54a816120de1

Fix TerminalControl crash on exit (#10031)
## Summary of the Pull Request

ControlCore's _renderer (IRenderTarget) is allocated as std::unique_ptr,
but is given to Terminal::CreateFromSettings as a reference.
ControlCore::Close deallocates the _renderer, but if ThrottledFuncs
are still scheduled to call ControlCore::UpdatePatternLocations
it'll cause Terminal::UpdatePatterns to be called, which in turn ends up
accessing the deallocated IRenderTarget reference and lead to a crash.

A proper solution with shared pointers is nontrivial and should be
attempted at a later point in time. This solution moves the teardown of
the _renderer into ControlCore::~ControlCore, where we can be certain
that no further strong references are held by ThrottledFuncs.

## PR Checklist
* [x] Closes #9910
* [x] I work here
* [x] Tests added/passed

## Validation Steps Performed

The crash is a race condition and inherently hard to reproduce.
During validation this PR didn't appear to introduce new crashes.

src/cascadia/TerminalControl/ControlCore.cpp
@@ -100,6 +100,29 @@ namespace winrt::Microsoft::Terminal::Control::implementation
     ControlCore::~ControlCore()
     {
         Close();
+
+        // Before destroying this instance we must ensure that we destroy the _renderer
+        // before the _renderEngine, as well as calling _renderer->TriggerTeardown().
+        // _renderEngine will be destroyed naturally after this ~destructor() returns.
+
+        decltype(_renderer) renderer;
+        {
+            // GH#8734:
+            // We lock the terminal here to make sure it isn't still being
+            // used in the connection thread before we destroy the renderer.
+            // However, we must unlock it again prior to triggering the
+            // teardown, to avoid the render thread being deadlocked. The
+            // renderer may be waiting to acquire the terminal lock, while
+            // we're waiting for the renderer to finish.
+            auto lock = _terminal->LockForWriting();
+
+            _renderer.swap(renderer);
+        }
+
+        if (renderer)
+        {
+            renderer->TriggerTeardown();
+        }
     }
 
     bool ControlCore::Initialize(const double actualWidth,
@@ -1164,30 +1187,6 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             // don't really care to wait for the connection to be completely
             // closed. We can just do it whenever.
             _asyncCloseConnection();
-
-            {
-                // GH#8734:
-                // We lock the terminal here to make sure it isn't still being
-                // used in the connection thread before we destroy the renderer.
-                // However, we must unlock it again prior to triggering the
-                // teardown, to avoid the render thread being deadlocked. The
-                // renderer may be waiting to acquire the terminal lock, while
-                // we're waiting for the renderer to finish.
-                auto lock = _terminal->LockForWriting();
-            }
-
-            if (auto localRenderEngine{ std::exchange(_renderEngine, nullptr) })
-            {
-                if (auto localRenderer{ std::exchange(_renderer, nullptr) })
-                {
-                    localRenderer->TriggerTeardown();
-                    // renderer is destroyed
-                }
-                // renderEngine is destroyed
-            }
-
-            // we don't destroy _terminal here; it now has the same lifetime as the
-            // control.
         }
     }
 

src/cascadia/UnitTests_TerminalCore/ScrollTest.cpp
@@ -45,7 +45,7 @@ namespace
         virtual void TriggerRedraw(const COORD* const){};
         virtual void TriggerRedrawCursor(const COORD* const){};
         virtual void TriggerRedrawAll(){};
-        virtual void TriggerTeardown(){};
+        virtual void TriggerTeardown() noexcept {};
         virtual void TriggerSelection(){};
         virtual void TriggerScroll(){};
         virtual void TriggerScroll(const COORD* const delta)

src/host/ScreenBufferRenderTarget.cpp
@@ -51,7 +51,7 @@ void ScreenBufferRenderTarget::TriggerRedrawAll()
     }
 }
 
-void ScreenBufferRenderTarget::TriggerTeardown()
+void ScreenBufferRenderTarget::TriggerTeardown() noexcept
 {
     auto* pRenderer = ServiceLocator::LocateGlobals().pRender;
     const auto* pActive = &ServiceLocator::LocateGlobals().getConsoleInformation().GetActiveOutputBuffer().GetActiveBuffer();

src/host/ScreenBufferRenderTarget.hpp
@@ -33,7 +33,7 @@ public:
     void TriggerRedraw(const COORD* const pcoord) override;
     void TriggerRedrawCursor(const COORD* const pcoord) override;
     void TriggerRedrawAll() override;
-    void TriggerTeardown() override;
+    void TriggerTeardown() noexcept override;
     void TriggerSelection() override;
     void TriggerScroll() override;
     void TriggerScroll(const COORD* const pcoordDelta) override;

src/renderer/base/renderer.cpp
@@ -319,7 +319,7 @@ void Renderer::TriggerRedrawAll()
 // - <none>
 // Return Value:
 // - <none>
-void Renderer::TriggerTeardown()
+void Renderer::TriggerTeardown() noexcept
 {
     // We need to shut down the paint thread on teardown.
     _pThread->WaitForPaintCompletionAndDisable(INFINITE);

src/renderer/base/renderer.hpp
@@ -52,7 +52,7 @@ namespace Microsoft::Console::Render
         void TriggerRedraw(const COORD* const pcoord) override;
         void TriggerRedrawCursor(const COORD* const pcoord) override;
         void TriggerRedrawAll() override;
-        void TriggerTeardown() override;
+        void TriggerTeardown() noexcept override;
 
         void TriggerSelection() override;
         void TriggerScroll() override;

src/renderer/inc/DummyRenderTarget.hpp
@@ -25,7 +25,7 @@ public:
     void TriggerRedraw(const COORD* const /*pcoord*/) override {}
     void TriggerRedrawCursor(const COORD* const /*pcoord*/) override {}
     void TriggerRedrawAll() override {}
-    void TriggerTeardown() override {}
+    void TriggerTeardown() noexcept override {}
     void TriggerSelection() override {}
     void TriggerScroll() override {}
     void TriggerScroll(const COORD* const /*pcoordDelta*/) override {}

src/renderer/inc/IRenderTarget.hpp
@@ -38,7 +38,7 @@ namespace Microsoft::Console::Render
         virtual void TriggerRedrawCursor(const COORD* const pcoord) = 0;
 
         virtual void TriggerRedrawAll() = 0;
-        virtual void TriggerTeardown() = 0;
+        virtual void TriggerTeardown() noexcept = 0;
 
         virtual void TriggerSelection() = 0;
         virtual void TriggerScroll() = 0;

commit aa157e55105aaae09c5ae3dfd4f53f40353e2367

Fix crash on exit introduced in ac265aa
ControlCore::AttachUiaEngine receives a IRenderEngine as a raw pointer,
which TermControl owns. We must ensure that we first destroy the
ControlCore before the UiaEngine instance (both owned by TermControl).
Otherwise a deallocated IRenderEngine is accessed when
ControlCore calls Renderer::TriggerTeardown.

src/cascadia/TerminalControl/ControlCore.cpp
@@ -104,24 +104,10 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         // Before destroying this instance we must ensure that we destroy the _renderer
         // before the _renderEngine, as well as calling _renderer->TriggerTeardown().
         // _renderEngine will be destroyed naturally after this ~destructor() returns.
-
-        decltype(_renderer) renderer;
+        
+        if (_renderer)
         {
-            // GH#8734:
-            // We lock the terminal here to make sure it isn't still being
-            // used in the connection thread before we destroy the renderer.
-            // However, we must unlock it again prior to triggering the
-            // teardown, to avoid the render thread being deadlocked. The
-            // renderer may be waiting to acquire the terminal lock, while
-            // we're waiting for the renderer to finish.
-            auto lock = _terminal->LockForWriting();
-
-            _renderer.swap(renderer);
-        }
-
-        if (renderer)
-        {
-            renderer->TriggerTeardown();
+            _renderer->TriggerTeardown();
         }
     }
 

src/cascadia/TerminalControl/ControlCore.h
@@ -171,9 +171,14 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         TerminalConnection::ITerminalConnection::StateChanged_revoker _connectionStateChangedRevoker;
 
         std::unique_ptr<::Microsoft::Terminal::Core::Terminal> _terminal{ nullptr };
-
-        std::unique_ptr<::Microsoft::Console::Render::Renderer> _renderer{ nullptr };
+        
+        // NOTE: _renderEngine must be ordered before _renderer.
+        //
+        // As _renderer has a dependency on _renderEngine (through a raw pointer)
+        // we must ensure the _renderer is deallocated first.
+        // (C++ class members are destroyed in reverse order.)
         std::unique_ptr<::Microsoft::Console::Render::DxEngine> _renderEngine{ nullptr };
+        std::unique_ptr<::Microsoft::Console::Render::Renderer> _renderer{ nullptr };
 
         IControlSettings _settings{ nullptr };
 

src/cascadia/TerminalControl/TermControl.h
@@ -130,22 +130,26 @@ namespace winrt::Microsoft::Terminal::Control::implementation
     private:
         friend struct TermControlT<TermControl>; // friend our parent so it can bind private event handlers
 
-        winrt::com_ptr<ControlCore> _core;
-        winrt::com_ptr<ControlInteractivity> _interactivity;
-
-        bool _initializedTerminal;
-
-        winrt::com_ptr<SearchBoxControl> _searchBox;
-
+        // NOTE: _uiaEngine must be ordered before _core.
+        //
+        // ControlCore::AttachUiaEngine receives a IRenderEngine as a raw pointer, which we own.
+        // We must ensure that we first destroy the ControlCore before the UiaEngine instance
+        // in order to safely resolve this unsafe pointer dependency. Otherwise a deallocated
+        // IRenderEngine is accessed when ControlCore calls Renderer::TriggerTeardown.
+        // (C++ class members are destroyed in reverse order.)
         std::unique_ptr<::Microsoft::Console::Render::UiaEngine> _uiaEngine;
 
+        winrt::com_ptr<ControlCore> _core;
+        winrt::com_ptr<ControlInteractivity> _interactivity;
+        winrt::com_ptr<SearchBoxControl> _searchBox;
+
         IControlSettings _settings;
-        bool _focused;
         std::atomic<bool> _closing;
+        bool _focused;
+        bool _initializedTerminal;
 
         std::shared_ptr<ThrottledFunc<>> _tsfTryRedrawCanvas;
         std::shared_ptr<ThrottledFunc<>> _updatePatternLocations;
-
         std::shared_ptr<ThrottledFunc<>> _playWarningBell;
 
         struct ScrollBarUpdate